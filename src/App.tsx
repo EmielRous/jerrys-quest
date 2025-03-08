38209f8e7740cd0bc2137f3aeb2b9d1b204de867
EmielRous <emielrous@hotmail.com>
2025-03-08 14:00:30 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import WikipediaLevel from "./levels/wikipedia_level/WikipediaLevel.tsx";
-// import LevelTwo from "./levels/level_two/LevelTwo.tsx";
-// import LevelThree from "./levels/level_three/LevelThree.tsx";
 import NavigationBar from "./components/NavigationBar.tsx";
+import DeskLevel from "./levels/desk_level/DeskLevel.tsx";
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
         <div className={"border border-red-100 h-[768px] w-[1024px]"}>
           <Routes>
             <Route path="/wikipedia-level" element={<WikipediaLevel />} />
-            <Route path="/level-two" element={<WikipediaLevel />} />
+            <Route path="/desk-level" element={<DeskLevel />} />
             <Route path="/level-three" element={<WikipediaLevel />} />
           </Routes>
         </div>
--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -6,21 +6,21 @@ const NavigationBar = () => {
     <div className="flex justify-around bg-gray-200 p-4">
       <Link to="/wikipedia-level">
         <img
-          src="public/wikipedia_level/Wiki-background.png"
+          src="wikipedia_level/Wiki-background.png"
           alt="Wikipedia Level"
           className="h-16 w-16"
         />
       </Link>
-      <Link to="/level-two">
+      <Link to="/desk-level">
         <img
-          src="public/wikipedia_level/Wiki-background.png"
+          src="wikipedia_level/Wiki-background.png"
           alt="Level Two"
           className="h-16 w-16"
         />
       </Link>
       <Link to="/level-three">
         <img
-          src="public/wikipedia_level/Wiki-background.png"
+          src="wikipedia_level/Wiki-background.png"
           alt="Level Three"
           className="h-16 w-16"
         />
