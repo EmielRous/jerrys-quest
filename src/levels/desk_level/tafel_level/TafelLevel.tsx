67b353e77dcd939be4ff848a7f8fe5f0ff0924d4
AineAakster <a.vandenaakster@studenti.unibs.it>
2025-03-09 15:50:38 +0100

    huehuueheuhe

--- src/levels/desk_level/tafel_level/TafelLevel.tsx
+++ src/levels/desk_level/tafel_level/TafelLevel.tsx
@@ -6,6 +6,10 @@ import BackButton from "../../../components/BackButton.tsx";
 const TafelLevel: React.FC = ({}) => {
   const navigate = useNavigate();
   const [damsetOpen, setDamsetOpen] = useState(false);
+  const [blik1Visible, setBlik1Visible] = useState(true);
+  const [blik2Visible, setBlik2Visible] = useState(false);
+  const [blik3Visible, setBlik3Visible] = useState(false);
+  const [blik4Visible, setBlik4Visible] = useState(false);
   const [pipeHover, setPipeHover] = useState(false);
 
   return (
@@ -16,24 +20,48 @@ const TafelLevel: React.FC = ({}) => {
         src="/desk_level/tafel_level/TroepTafelView.png"
       />
       <ClickableImage
+          visible={blik1Visible}
         path="/desk_level/tafel_level/Blikje1.png"
         size={{ w: 142, h: 150 }}
         location={{ x: 594, y: 166 }}
+          clickable={true}
+          onClick={() => setBlik1Visible(false)}
+          onClick={() => setBlik2Visible(true)}
+          onClick={() => setBlik3Visible(false)}
+          onClick={() => setBlik4Visible(false)}
       />
       <ClickableImage
+          visible={blik2Visible}
         path="/desk_level/tafel_level/Blikje2.png"
         size={{ w: 142, h: 150 }}
         location={{ x: 594, y: 166 }}
+          clickable={true}
+          onClick={() => setBlik1Visible(false)}
+          onClick={() => setBlik2Visible(false)}
+          onClick={() => setBlik3Visible(true)}
+          onClick={() => setBlik4Visible(false)}
       />
       <ClickableImage
+          visible={blik3Visible}
         path="/desk_level/tafel_level/Blikje3.png"
         size={{ w: 142, h: 150 }}
         location={{ x: 594, y: 166 }}
+          clickable={true}
+          onClick={() => setBlik1Visible(false)}
+          onClick={() => setBlik2Visible(false)}
+          onClick={() => setBlik3Visible(false)}
+          onClick={() => setBlik4Visible(true)}
       />
         <ClickableImage
+            visible={blik4Visible}
             path="/desk_level/tafel_level/Blikje4.png"
             size={{ w: 142, h: 150 }}
             location={{ x: 594, y: 166 }}
+            clickable={true}
+            onClick={() => setBlik1Visible(true)}
+            onClick={() => setBlik2Visible(false)}
+            onClick={() => setBlik3Visible(false)}
+            onClick={() => setBlik4Visible(false)}
         />
         <ClickableImage
             path="/desk_level/tafel_level/Envelop.png"
