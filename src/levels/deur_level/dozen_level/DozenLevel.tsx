bddc8c5f579e1927a6ba610ca61b98a7f126bbdf
AineAakster <a.vandenaakster@studenti.unibs.it>
2025-03-09 14:36:01 +0100

    huehuueheuhe

--- src/levels/deur_level/dozen_level/DozenLevel.tsx
+++ src/levels/deur_level/dozen_level/DozenLevel.tsx
@@ -13,53 +13,53 @@ const DozenLevel: React.FC = () => {
 
       <ClickableImage
         path="/deur_level/dozen_level/Doos1.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 50, y: 50 }}
+        size={{ w: 400, h: 500 }}
+        location={{ x: 8, y: 100 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Doos2.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 200, y: 50 }}
+        size={{ w: 500, h: 400 }}
+        location={{ x: 301, y: 211 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Doos3.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 350, y: 50 }}
+        size={{ w: 500, h: 450 }}
+        location={{ x: 93, y: 150 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Doos4.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 500, y: 50 }}
+        size={{ w: 700, h: 600 }}
+        location={{ x: 319, y: 20 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Koning.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 650, y: 50 }}
+        size={{ w: 200, h: 400 }}
+        location={{ x: 33, y: 202 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Lidl.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 800, y: 50 }}
+        size={{ w: 377, h: 466 }}
+        location={{ x: 415, y: 157 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Paddos.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 950, y: 50 }}
+        size={{ w: 152, h: 143 }}
+        location={{ x: 821, y: 455 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Shelf.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 50, y: 200 }}
+        size={{ w: 1024, h: 199 }}
+        location={{ x: 0, y: 547 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Slaapzak.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 200, y: 200 }}
+        size={{ w: 643, h: 435 }}
+        location={{ x: 200, y: 173 }}
       />
       <ClickableImage
         path="/deur_level/dozen_level/Vraag.png"
-        size={{ w: 100, h: 100 }}
-        location={{ x: 350, y: 200 }}
+        size={{ w: 320, h: 268 }}
+        location={{ x: 158, y: 35 }}
       />
     </div>
   );
