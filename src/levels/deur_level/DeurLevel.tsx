e5dad5df494e065176d631680af95dc6b1867616
AineAakster <a.vandenaakster@studenti.unibs.it>
2025-03-09 13:27:06 +0100

    huehuueheuhe

--- src/levels/deur_level/DeurLevel.tsx
+++ src/levels/deur_level/DeurLevel.tsx
@@ -13,64 +13,6 @@ const BureauLevel: React.FC = ({}) => {
         className={"relative h-[768px] w-[1024px]"}
         src={"/deur_level/DeurView.png"}
       />
-      <ClickableImage
-        path="/deur_level/BankAfter.png"
-        size={{ w: 300, h: 250 }}
-        location={{ x: 230, y: 267 }}
-      />
-
-      <ClickableImage
-        path="/deur_level/BankBefore.png"
-        size={{ w: 400, h: 230 }}
-        location={{ x: 258, y: 256 }}
-      />
-      <ClickableImage
-        path="/deur_level/Boeken.png"
-        size={{ w: 152, h: 196 }}
-        location={{ x: 505, y: 255 }}
-      />
-      <ClickableImage
-        path="/deur_level/Dozen.png"
-        size={{ w: 80, h: 65 }}
-        clickable={true}
-        location={{ x: 609, y: 444 }}
-        redirect={DeurLevelPaths.DozenLevel}
-      />
-
-      <ClickableImage
-        path="/deur_level/Gordijn.png"
-        size={{ w: 230, h: 300 }}
-        location={{ x: 120, y: 360 }}
-      />
-      <ClickableImage
-        path="/deur_level/Kleren.png"
-        size={{ w: 230, h: 120 }}
-        location={{ x: 120, y: 550 }}
-      />
-
-      <ClickableImage
-        path="/deur_level/Lamp.png"
-        size={{ w: 330, h: 300 }}
-        location={{ x: 660, y: 290 }}
-      />
-
-      <ClickableImage
-        path="/deur_level/Plank.png"
-        size={{ w: 200, h: 100 }}
-        location={{ x: 660, y: 290 }}
-      />
-
-      <ClickableImage
-        path="/deur_level/Poef.png"
-        size={{ w: 200, h: 100 }}
-        location={{ x: 660, y: 290 }}
-      />
-
-      <ClickableImage
-        path="/deur_level/Projector.png"
-        size={{ w: 200, h: 100 }}
-        location={{ x: 660, y: 290 }}
-      />
 
       <ClickableImage
         path="/deur_level/TapijtBefore.png"
@@ -78,6 +20,58 @@ const BureauLevel: React.FC = ({}) => {
         location={{ x: 660, y: 290 }}
         clickable
         redirect={DeurLevelPaths.TapijtLevel}
+      />
+      <ClickableImage
+        path="/deur_level/BankAfter.png"
+        size={{ w: 466, h: 250 }}
+        location={{ x: 14, y: 600 }}
+      />
+      <ClickableImage
+        path="/deur_level/BankBefore.png"
+        size={{ w: 466, h: 250 }}
+        location={{ x: 14, y: 600 }}
+      />
+      <ClickableImage
+        path="/deur_level/Dozen.png"
+        size={{ w: 314, h: 131 }}
+        location={{ x: 542, y: 104 }}
+        clickable={true}
+        redirect={DeurLevelPaths.DozenLevel}
+      />
+      <ClickableImage
+        path="/deur_level/Gordijn.png"
+        size={{ w: 230, h: 583 }}
+        location={{ x: -24, y: 52 }}
+      />
+      <ClickableImage
+        path="/deur_level/Kleren.png"
+        size={{ w: 230, h: 120 }}
+        location={{ x: 349, y: 475 }}
+      />
+      <ClickableImage
+        path="/deur_level/Lamp.png"
+        size={{ w: 100, h: 100 }}
+        location={{ x: 110, y: 434 }}
+      />
+      <ClickableImage
+        path="/deur_level/Poef.png"
+        size={{ w: 200, h: 190 }}
+        location={{ x: 431, y: 676 }}
+      />
+      <ClickableImage
+        path="/deur_level/Projector.png"
+        size={{ w: 154, h: 54 }}
+        location={{ x: 310, y: 183 }}
+      />
+      <ClickableImage
+        path="/deur_level/Boeken.png"
+        size={{ w: 131, h: 73 }}
+        location={{ x: 476, y: 691 }}
+      />
+      <ClickableImage
+        path="/deur_level/Plank.png"
+        size={{ w: 724, h: 147 }}
+        location={{ x: 152, y: 210 }}
       />
     </div>
   );
