07e07debb6d43ca46d6ae0efb88214fbfd79b367
AineAakster <a.vandenaakster@studenti.unibs.it>
2025-03-12 03:53:22 +0100

    huehuehuehue

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BackgroundImage: React.FC = () => {
     return (
         <img
-            src="./Background.png"
+            src="/background.png"
             alt="Background"
             className="absolute top-0 right-0 h-screen w-auto"
             style={{ maxWidth: "none" }}
